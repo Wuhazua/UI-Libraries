0896941a9ab604c39b05ad626338b4f87de19e6a
Wuhazu <wuhazu18@gmail.com>
2024-07-13 18:03:44 -0800

    Update Example.lua

--- informant.wtf Lib/Example.lua
+++ informant.wtf Lib/Example.lua
@@ -1,176 +1,143 @@
-local Decimals = 4
-local Clock = os.clock()
-local ValueText = "Value Is Now :"
+-- Credits To The Original Devs @xz, @goof
+getgenv().Config = {
+	Invite = "informant.wtf",
+	Version = "0.0",
+}
 
-local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/drillygzzly/Roblox-UI-Libs/main/1%20Tokyo%20Lib%20(FIXED)/Tokyo%20Lib%20Source.lua"))({
-    cheatname = "Title Here", -- watermark text
-    gamename = "Title Here", -- watermark text
+getgenv().luaguardvars = {
+	DiscordName = "username#0000",
+}
+
+local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/drillygzzly/Other/main/1"))()
+
+library:init() -- Initalizes Library Do Not Delete This
+
+local Window = library.NewWindow({
+	title = "Informant.Wtf",
+	size = UDim2.new(0, 525, 0, 650)
 })
 
-library:init()
+local tabs = {
+    Tab1 = Window:AddTab("Tab1"),
+	Settings = library:CreateSettingsTab(Window),
+}
 
-local Window1  = library.NewWindow({
-    title = "Title Here | Title Here", -- Mainwindow Text
-    size = UDim2.new(0, 510, 0.6, 6
-)})
+-- 1 = Set Section Box To The Left
+-- 2 = Set Section Box To The Right
 
-local Tab1 = Window1:AddTab("  Tab1  ")
-local SettingsTab = library:CreateSettingsTab(Window1)
+local sections = {
+	Section1 = tabs.Tab1:AddSection("Section1", 1),
+	Section2 = tabs.Tab1:AddSection("Section2", 2),
+}
 
---Tab1:SetText("Text")
-
-local Section1 = Tab1:AddSection("Section 1", 1)
---Section1:SetText("Text")
-
-Section1:AddToggle({
-    text = "Toggle1",
-    state = false,
-    risky = true,
-    tooltip = "tooltip1",
-    flag = "Toggle_1",
-    risky = false,
-    callback = function(v)
-        print(ValueText, v)
-    end
-}):AddBind({
-    enabled = true,
-    text = "Keybind1",
-    tooltip = "tooltip1",
-    mode = "toggle",
-    bind = "None",
-    flag = "ToggleKey_1",
-    state = false,
-    nomouse = false,
-    risky = false,
-    noindicator = false,
-    callback = function(v)
-        print(ValueText, v)
-    end,
-    keycallback = function(v)
-        print(ValueText, v)
-    end
+sections.Section1:AddToggle({
+	enabled = true,
+	text = "Toggle1",
+	flag = "Toggle_1",
+	tooltip = "Tooltip1",
+	risky = true, -- turns text to red and sets label to risky
+	callback = function(lol)
+	    print("Toggle Is Now Set To : ".. lol)
+	end
 })
 
-Section1:AddBox({
+sections.Section1:AddButton({
+	enabled = true,
+	text = "Button1",
+	flag = "Button_1",
+	tooltip = "Tooltip1",
+	risky = false,
+	confirm = false, -- shows confirm button
+	callback = function(v)
+	    print(v)
+	end
+})
+
+sections.Section1:AddSeparator({
+	text = "Separator"
+})
+
+sections.Section1:AddSlider({
+	text = "Slider", 
+	flag = 'Slider_1', 
+	suffix = "", 
+	value = 0.000,
+	min = 0.1, 
+	max = 0.999,
+	increment = 0.001,
+	tooltip = "Tooltip1",
+	risky = false,
+	callback = function(v) 
+		print("Slider Value Is Now : ".. v)
+	end
+})
+
+sections.Section1:AddBind({
+	text = "Keybind",
+	flag = "Key_1",
+	nomouse = true,
+	noindicator = true,
+	tooltip = "Tooltip1",
+	mode = "toggle",
+	bind = Enum.KeyCode.Q,
+	risky = false,
+	keycallback = function(v)
+	    print("Keybind Changed!")
+	end
+})
+
+sections.Section1:AddList({
+	enabled = true,
+	text = "List",
+	flag = "List_1",
+	multi = false,
+	tooltip = "Tooltip1",
+    risky = false,
+    dragging = false,
+    focused = false,
+	value = "1",
+	values = {
+		"1",
+		"2",
+		"3"
+	},
+	callback = function(v)
+	    print("List Value Is Now : "..v)
+	end
+})
+
+sections.Section1:AddBox({
     enabled = true,
-    name = "TextBox1",
-    flag = "TextBox_1",
+    focused = true,
+    text = "TextBox1",
     input = "PlaceHolder1",
-    focused = false,
-    risky = false,
-    callback = function(v)
-        print(ValueText, v)
-    end
+	flag = "Text_1",
+	risky = false,
+	callback = function(v)
+	    print(v)
+	end
 })
 
---[[Section1:AddToggle({
-    text = "Toggle1",
-    flag = "Toggle_1",
-    callback = function(v)
-        print(ValueText, v)
-    end
-}):AddColor({
-    text = "Color1",
-    color = Color3.fromRGB(255, 255, 255),
+sections.Section1:AddText({
+    enabled = true,
+    text = "Text1",
+    flag = "Text_1",
+    risky = false,
+})
+
+sections.Section1:AddColor({
+    enabled = true,
+    text = "ColorPicker1",
     flag = "Color_1",
-    callback = function(v)
-        print(ValueText, v)
-    end
-})--]]
-
---[[Section1:AddBind({
-    enabled = true,
-    text = "Keybind1",
-    tooltip = "tooltip1",
-    mode = "toggle",
-    bind = "None",
-    flag = "ToggleKey_1",
-    state = false,
-    nomouse = false,
-    risky = false,
-    noindicator = false,
-    callback = function(v)
-        print(ValueText, v)
-    end,
-    keycallback = function(v)
-        print(ValueText, v)
-    end
-})--]]
-
-Section1:AddSeparator({
-    enabled = true,
-    text = "Separator1"
-})
-
-Section1:AddButton({
-    enabled = true,
-    text = "Button1",
-    tooltip = "tooltip1",
-    confirm = true,
-    risky = false,
-    callback = function()
-        print("Pressed!")
-    end
-})
-
--- Button1:SetText("Text")
-
-Section1:AddSlider({
-    enabled = true,
-    text = "Slider1",
-    tooltip = "tooltip1",
-    flag = "Slider_1",
-    suffix = "",
-    dragging = true,
-    focused = false,
-    min = 0,
-    max = 100,
-    increment = 0.1,
-    risky = false,
-    callback = function(v)
-        print(ValueText, v)
-    end
-})
-
---Slider_1:SetValue(100)
-
-Section1:AddList({
-    enabled = true,
-    text = "Selection", 
-    tooltip = "tooltip1",
-    selected = "",
-    multi = false,
-    open = false,
-    max = 4,
-    values = {"1", "2", "3"},
-    risky = false,
-    callback = function(v)
-        print(ValueText, v)
-    end
-})
-
-Section1:AddColor({
-    enabled = true,
-    text = "Color1",
-    tooltip = "tooltip1",
-    color = Color3.fromRGB(255, 255, 255),
-    flag = "Color_1",
+    tooltip = "ToolTip1",
+    color = Color3.new(255, 255, 255),
     trans = 0,
     open = false,
-    risky = false,
-    callback = function(v)
-        print(ValueText, v)
+    callback = function()
+        
     end
 })
 
-local Time = (string.format("%."..tostring(Decimals).."f", os.clock() - Clock))
-library:SendNotification(("Loaded In "..tostring(Time)), 6)
+library:SendNotification("Notification", 5, Color3.new(255, 0, 0))
 
---[[
-    --Window1:SetOpen(false)
-    makefolder("Title Here")
-    library:SetTheme(Default)
-    library:GetConfig(Default)
-    library:LoadConfig(Default)
-    library:SaveConfig(Default)
-]]
+--Window:SetOpen(true) -- Either Close Or Open Window
